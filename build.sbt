e7bd955600d011d9b5104e84620af0ebd4448cfb
aprox13 <ifkbhit@gmail.com>
2020-10-22 13:25:22 +0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ppo-lab6"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/ru/ifkbhit/ppo6/Main.scala
@@ -0,0 +1,7 @@
+package ru.ifkbhit.ppo6
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+}
